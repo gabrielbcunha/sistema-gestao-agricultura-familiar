128748125ec66065e5ad4d4128a068e8764b9ea7
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-02-02 18:07:14 -0300

    feat: implementa classe AreaCultivo com associação a Produtor e validações

---
+++ src/AreaCultivo.java
@@ -0,0 +1,54 @@
+public class AreaCultivo {
+    private final int idArea;
+    private Produtor produtor;
+    private String nomeArea;
+    private String tamanhoArea;
+
+    public AreaCultivo(int idArea, Produtor produtor, String nomeArea, String tamanhoArea ) {
+
+        if (idArea <= 0){
+            throw new IllegalArgumentException("idArea deve ser positivo");
+        }
+        if (produtor == null){
+            throw new IllegalArgumentException("produtor deve existir");
+        }
+        if (nomeArea == null || nomeArea.isBlank()){
+            throw new IllegalArgumentException("Area deve ter um nome");
+        }
+        if (tamanhoArea == null || tamanhoArea.isBlank()){
+            throw new IllegalArgumentException("Area deve ter um tamanho");
+        }
+
+        this.idArea = idArea;
+        this.produtor = produtor;
+        this.nomeArea = nomeArea;
+        this.tamanhoArea = tamanhoArea;
+    }
+
+    public int getIdArea() {
+        return idArea;
+    }
+
+    public String getTamanhoArea() {
+        return tamanhoArea;
+    }
+
+    public String getNomeArea() {
+        return nomeArea;
+    }
+
+    public Produtor getProdutor() {
+        return produtor;
+    }
+
+    public int getIdProdutor() {
+        return produtor.getIdProdutor();
+    }
+
+    public void alterarProdutor(Produtor produtor) {
+        if (produtor == null){
+            throw new IllegalArgumentException("produtor deve existir");
+        }
+        this.produtor =  produtor;
+    }
+}
